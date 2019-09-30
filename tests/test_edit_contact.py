6aea50402dd1a4351ef911c7cb4aa10dd3f349cb
Zhilkina Yulia <yzhilkina@ozon.ru>
2019-09-30 16:50:16 +0300

    добавлены предусловия

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -3,12 +3,18 @@ from model.contact import Contact
 
 
 def test_edit_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(firstname="New First name", middlename="New Middle name", lastname="New Last name"))
 
 
 def test_edit_address(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(address="New Company Address", address2="New Address"))
 
 
 def test_edit_bdate(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(bday="22", bmonth="July", byear="2000"))
--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -3,12 +3,18 @@ from model.group import Group
 
 
 def test_edit_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="New_Group"))
 
 
 def test_edit_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(header="new_header"))
 
 
 def test_edit_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(footer="new_footer"))
